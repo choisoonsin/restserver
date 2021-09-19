cf33a20c3868c7be48c40b7b6d71dec8ca6f9727
choisoonsin <cyh0214@gmail.com>
2021-09-19 15:52:10 +0900

    Done step1 for spring boot + jpa + mysql

--- src/main/java/com/jpa/example/demo/controller/BookController.java
+++ src/main/java/com/jpa/example/demo/controller/BookController.java
@@ -4,9 +4,17 @@ import com.jpa.example.demo.model.Book;
 import com.jpa.example.demo.model.BookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpServerErrorException;
 
 @Controller
+@RequestMapping("/book")
 public class BookController {
 
   private BookRepository bookRepository;
@@ -16,7 +24,20 @@ public class BookController {
     this.bookRepository = bookRepository;
   }
 
-  public Iterable<Book> selectAllBooks() {
+  @PostMapping("/create")
+  public @ResponseBody Book insertBook(@RequestBody Book param) {
+    if ( param.getName().isEmpty() ) {
+      throw new HttpServerErrorException(HttpStatus.BAD_REQUEST);
+    }
+
+    Book newBook = new Book();
+    newBook.setName(param.getName());
+
+    return bookRepository.save(newBook);
+  }
+
+  @GetMapping("/selectAll")
+  public @ResponseBody Iterable<Book> selectAllBooks() {
     return bookRepository.findAll();
   }
   
