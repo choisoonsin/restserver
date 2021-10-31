be088abd03b2d9256530f1e226d255e597768a10
choisoonsin <cyh0214@gmail.com>
2021-10-31 11:19:17 +0900

    Add lombok dependencies

--- src/main/java/com/jpa/example/demo/model/Book.java
+++ src/main/java/com/jpa/example/demo/model/Book.java
@@ -6,38 +6,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
 @Entity
+@Table(name = "book")
+@NoArgsConstructor
 public class Book {
-  
+
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private Date publishedDateOn;
-
-  public Long getId() {
-    return id;
-  }
-
-  public Date getPublishedDateOn() {
-    return publishedDateOn;
-  }
-
-  public void setPublishedDateOn(Date publishedDateOn) {
-    this.publishedDateOn = publishedDateOn;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 
 }
